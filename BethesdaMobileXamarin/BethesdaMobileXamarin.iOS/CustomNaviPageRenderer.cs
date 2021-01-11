58f3b246d9110c3c77614499f11afd20544eb560
RuswendraAditya <wendra.adityagraha@gmail.com>
2021-01-11 13:58:30 +0700

    -- cek dark mode xamarin

---
+++ BethesdaMobileXamarin/BethesdaMobileXamarin.iOS/CustomNaviPageRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BethesdaMobileXamarin.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomNaviPageRenderer))]
+namespace BethesdaMobileXamarin.iOS
+{
+    public class CustomNaviPageRenderer : NavigationRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+        }
+    }
+}
---
+++ BethesdaMobileXamarin/BethesdaMobileXamarin.iOS/CustomPageRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BethesdaMobileXamarin.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CustomPageRenderer))]
+namespace BethesdaMobileXamarin.iOS
+{
+    public class CustomPageRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+        }
+    }
+}
