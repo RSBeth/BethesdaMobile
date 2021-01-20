89d915249b3a9501676c8f244d87838325bdc6ae
RuswendraAditya <wendra.adityagraha@gmail.com>
2021-01-20 09:27:37 +0700

    -- Perbaikan dark mode

---
+++ BethesdaMobileXamarin/BethesdaMobileXamarin.iOS/DialogAlertPageRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using Rg.Plugins.Popup.IOS.Renderers;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace BethesdaMobileXamarin.iOS
+{
+    public class DialogAlertPageRenderer : PopupPageRenderer
+    {
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+        }
+    }
+}
