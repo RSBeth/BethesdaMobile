2485b8e91fca1c4bb3e8b692fd8216358d10b74b
RuswendraAditya <wendra.adityagraha@gmail.com>
2021-01-20 09:43:55 +0700

    -perbaikan dark mode 20 januari 2020

--- BethesdaMobileXamarin/BethesdaMobileXamarin.iOS/DialogAlertPageRenderer.cs
+++ BethesdaMobileXamarin/BethesdaMobileXamarin.iOS/DialogAlertPageRenderer.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using BethesdaMobileXamarin.iOS;
 using Foundation;
 using Rg.Plugins.Popup.IOS.Renderers;
 using UIKit;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
-
+[assembly: ExportRenderer(typeof(PopupPageRenderer), typeof(DialogAlertPageRenderer))]
 namespace BethesdaMobileXamarin.iOS
 {
     public class DialogAlertPageRenderer : PopupPageRenderer
